d6378f6cb9a9251738293da8ac721118f21f4fcb
Pascaline Dah Zinsou <pascaline@wazoom-studio.com>
2024-03-19 18:29:04 +0100

    Update teams.

--- resources/views/pages/teams.blade.php
+++ resources/views/pages/teams.blade.php
@@ -3,75 +3,46 @@
 <section class="section-box">
   <div class="banner-hero banner-breadcrums">
     <div class="container text-center">
-      <h1 class="text-heading-2 color-gray-1000 mb-20">Notre Equipe</h1>
-      <p class="text-body-text color-gray-500">Equidem necessitatibus ei eam, ceteros expetenda<br class="d-lg-block d-none"> hendrerit ei per, tation vituperatoribus ut.</p>
+      <h1 class="text-heading-2 color-gray-1000 mb-20" _msttexthash="160706" _msthash="36">Notre Equipe</h1>
+      <p class="text-body-text color-gray-500">Avec passion et créativité notre équipe met à votre disposition ses compétences et son expérience<br class="d-lg-block d-none"> afin de garantir la réussite de vos divers projets.</p>
     </div>
   </div>
 </section>
 <section class="section-box">
   <div class="container mt-110">
     <div class="row">
-      <div class="col-lg-9 col-sm-8">
-        <h3 class="text-heading-1 mb-10">Our Team</h3>
-        <p class="text-body-lead-large color-gray-600">Decades of experience in design and development</p>
-      </div>
+      <div class="col-lg-100 col-sm-99" style="text-justify">
+        <h3 class="text-heading-1 mb-10">Notre Equipe</h3>
+        <p class="text-body-lead-large color-gray-600">Votre réussite, en tant que client ou partenaire, est aussi la nôtre; car à CBOX Sarl nous pensons que la réussite doit toujours être collective. Prenez contact avec nous dès aujourd’hui pour vos projets en développement logiciel, site internet et tout autre besoin situé dans nos domaines d’activité.</p>      </div>
     </div>
   </div>
   <div class="container mt-80">
     <div class="row">
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-7.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
+          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-7.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Directeur Général</span>
+          <h3 class="text-heading-5 mb-5 mt-5">TONI Firmin</h3>
           <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-3.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
+          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-3.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Technicien</span>
+          <h3 class="text-heading-5 mb-5 mt-5">ABI TONI Romaric</h3>
           <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-4.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
+          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-4.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Developpeur</span>
+          <h3 class="text-heading-5 mb-5 mt-5">ADOGONI Ulrich</h3>
           <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
         </div>
       </div>
       <div class="col-lg-3 col-md-6 col-sm-6">
         <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-5.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
+          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-5.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Developpeur</span>
           <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
-          <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6">
-        <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-6.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
-          <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6">
-        <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-7.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
-          <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-6">
-        <div class="card-grid-style-5 hover-up">
-          <div class="grid-5-img mb-15"><img src="assets/imgs/page/about/1/team-8.png" alt="Agon"></div><span class="text-body-text-md color-gray-500">Marketing CEO</span>
-          <h3 class="text-heading-5 mb-5 mt-5">Theresa Webb</h3>
-          <p class="text-body-excerpt text-desc color-gray-500 mt-15 mb-20">Lorem ipsum dolor sit amet consectetur imp adipiscing elit justo</p><span class="text-body-text-md color-gray-600">Let&rsquo;s Connect</span>
           <div class="social-bottom"><a class="icon-socials icon-facebook" href="#"></a><a class="icon-socials icon-twitter" href="#"></a><a class="icon-socials icon-instagram" href="#"></a><a class="icon-socials icon-linkedin" href="#"></a></div>
         </div>
       </div>
